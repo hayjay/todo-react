ee076875b02783dc41d1ea3ff1bc2c4d1464e653
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-10-25 18:52:37 +0100

    changes on note component and rendered

--- src/components/Note.js
+++ src/components/Note.js
@@ -11,15 +11,29 @@ class Note extends Component {
 
   componentDidMount(){
     fetch('http://localhost:4000/tasks').then(results => {
-      return results.json();
-    });
+          return results.json();
+        })
+        .then(data => {
+          let notes = data.results.map((each_note) => {
+            return (
+              <div className="note" key={each_note.results} onClick={this.props.deleteMethod}>
+                {each_note.results.name}
+              </div>
+            )
+          });
+          //set or update the state (calling this.setState) after pulling notes from the api
+          this.setState({notes : notes});
+          console.log("todos", this.state.notes)
+        });
   }
 
 
   render() {
     return (
       <div className="note" onClick={this.props.deleteMethod}>
-        {this.props.text}
+        {/* //render the pulled data (from api) to the page/view */ }
+        {/* below, we can call this.state.pictures because we have updated the state using this.setState in the componentDidMount function */}
+        {this.state.pictures}
       </div>
     );
   }
