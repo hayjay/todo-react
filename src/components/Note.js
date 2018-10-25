ae0d3fa48a7e81bd202b68e8865872487e53cc67
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-10-25 18:21:43 +0100

    implementing api cal

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class Note extends Component {
+  constructor () {
+    super();
+
+    this.state = { //set component initial state
+      notes : []
+    };
+  };
+
   
   render() {
     return (
