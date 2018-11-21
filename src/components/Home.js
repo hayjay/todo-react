6c07b3a15a955993dbaba8d5cb9c810a76be89b4
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-11-21 15:51:48 +0100

    made sticky navbar absolute and fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,16 @@ const Home = () => {
     return (
         <div>
             <p>
-                
+                <form method="" action="">
+                    <label >Name : </label>
+                    <input type="text" name="name" id="name"></input>
+
+                    <label >Email : </label>
+                    <input type="text" name="email" id="email"></input>
+                    
+                    <label >Password : </label>
+                    <input type="text" name="password" id="password"></input>
+                </form>
             </p>
         </div>
     )
