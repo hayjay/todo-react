60e183d520b3cebfbc48dc8ccad3c8fd8c012ac4
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-11-22 11:18:26 +0100

    Added handleRegistration function

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,26 @@ import React from "react";
 import '../Home.css'
 
 const Home = () => {
+
+    handleRegistration (e) {
+        e.preventDefault();
+    }
+
     return (
         <div className="container">
            <input type='checkbox' id='form-switch'/>
-            <form id='login-form' action="" method='post'>
-            <input type="text" placeholder="Username" required/>
-            <input type="password" placeholder="Password" required/>
-            <button type='submit'>Login</button>
-            <label for='form-switch'><span>Register</span></label>
+            <form id='login-form' >
+                <input type="text" placeholder="Username" required/>
+                <input type="password" placeholder="Password" required/>
+                <button type='submit'>Login</button>
+                <label for='form-switch'><span>Register</span></label>
             </form>
-            <form id='register-form' action="" method='post'>
+            <form id='register-form' onSubmit={ this.handleRegistration }>
             <input type="text" placeholder="Username" required/>
             <input type="email" placeholder="Email" required/>
             <input type="password" placeholder="Password" required/>
             <input type="password" placeholder="Re Password" required/>
-            <button type='submit'>Register</button>
+            <button type='submit' onClick={this.handleRegistration.bind(this) } >Register</button>
             <label for='form-switch'>Already Member ? Sign In Now..</label>
             </form>
         </div>
